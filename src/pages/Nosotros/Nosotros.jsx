d230bcdead04f05bf3002b46e54b2d103c5f4159
Jhon Oblitas <134904618+Zxpty@users.noreply.github.com>
2024-02-03 21:17:32 -0500

    feat: add more sections, and images for the page nosotros

--- src/pages/Nosotros/Nosotros.jsx
+++ src/pages/Nosotros/Nosotros.jsx
@@ -2,18 +2,19 @@
 import TitleSpace from '../../Components/TitleSpace/TitleSpace';
 import '../../index.css';
 import './Nosotros.css'
+
 export default function Nosotros() {
     return (
         <>
             <TitleSpace
-                firstTitle="¡Bienvenidos Al"
-                secondTitle="Colegio Galileo!"
+                firstTitle="Conoce"
+                secondTitle="Nuestra Historia"
             />
-            <section id="hero">
-                <div>
-                    <img src="" alt="" />
+            <section className='section-historia'>
+                <div className='historia-image'>
+                    <img src="./src/assets/images/historia1.png" alt="" />
                 </div>
-                <div>
+                <div className='historia-content'>
                     El inicio de nuestro Colegio Privado Galileo se realiza el 13 de diciembre de 2022
                     en la hermosa ciudad de Huacho, Perú. Desde entonces, nos hemos comprometido apasionadamente
                     a brindar una educación de excelencia, formando mentes brillantes con sólidos principios y valores.
@@ -27,6 +28,20 @@ export default function Nosotros() {
                 firstTitle="Nuestra"
                 secondTitle="Misión y Visión"
             />
+            <div className='section-vision-mision'>
+                <div className='mision'>
+                    <div>
+                    <img src="./src/assets/images/MISION.png" alt="" />
+                    <h1>MISIÓN</h1>
+                    </div>
+                </div>
+                <div className='vision'>
+                    <div>
+                    <img src="./src/assets/images/VISION1.png" alt="" />
+                    <h1>VISIÓN</h1>
+                    </div>
+                </div>
+            </div>
             <TitleSpace
                 firstTitle="Conoce"
                 secondTitle="Nuestros Pilares"
